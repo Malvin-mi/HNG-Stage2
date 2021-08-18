6a71a76cacb5aeeb98edb3e32f0530d79c9363ed
Malvin-mi <michaelogunyemi3@gmail.com>
2021-08-19 00:09:07 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const button = document.getElementById("button");
+const myName = document.querySelector("#name");
+
+button.addEventListener('click', () => {
+    myName.style.display = "block";
+})
